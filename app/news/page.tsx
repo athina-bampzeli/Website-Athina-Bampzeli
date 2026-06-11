a375eb6425a1ba58a2b1ac15436fb7f1b25addf1
Athina Bampzeli <athbabzeli@gmail.com>
2026-06-11 15:34:23 +0300

    change27

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -14,56 +14,10 @@ export default function News() {
         <h2 className="text-3xl font-bold mb-6">News</h2>
 
         <div className="flex justify-center">
-          <div className="grid md:grid-cols-2 gap-20 max-w-4xl w-full">
+          <div className="grid md:grid-cols-2 gap-6 space-y-6 max-w-4xl">
 
         
-            {/* pesxm15 */}
-            <FlipCard
-              front={
-                
-                
-                <div className="absolute inset-0 flex flex-col items-center justify-center border rounded-xl shadow-sm group-hover:shadow-md p-4 text-center">
-                  <h3 className="text-2xl font-semibold">
-                    15th Panhellenic Scientific Conference on Chemical Engineering
-                  </h3>
-
-                    <div className="mt-3 text-lg font-normal text-gray-600">
-                      3-5 June 2026 <br />
-                      Technical University of Crete, Chania, Greece
-                    </div>
-                  
-                </div>
-              }
-              back={
-
-                
-                <div className="absolute inset-0 flex flex-col items-center justify-center border rounded-xl bg-gray-100 p-4 text-center">
-                  <p className="text-gray-700">The conference will focus on highlighting interdisciplinary areas
-                    of Chemical Engineering, as
-                    well as current trends, recent developments, and research in fields such as Biochemical
-                    Engineering, Biomedical Engineering, Nanotechnology, Electrochemistry, Green Chemistry,
-                    Sustainable and Circular Development, Machine Learning, Safety and Hygiene in Chemical
-                    Processes, Environmental Protection, Energy Transition, and the mitigation of Climate Change
-                    impacts.
-                  </p>
-                  <p className="text-gray-700">
-                    Visit{" "}
-                    <a
-                      href="https://pesxm15.tuc.gr/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-600 hover:underline font-medium"
-                    >
-                      pesxm15 website
-                    </a>{" "}
-                    for more.
-                  </p>
-
-                  
-                </div>
-
-              }
-            />
+            
 
             {/* datahow course */}
             <FlipCard
